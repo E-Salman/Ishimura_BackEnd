0ef3ca5845f66135d76a1ccb8ed946cd28b87b19
bfmasiero <bmasiero@uade.edu.ar>
2025-09-02 12:56:41 -0300

    Armo Orden.java

--- src/main/java/Ishimura/uade/IshimuraCollectibles/entity/Orden.java
+++ src/main/java/Ishimura/uade/IshimuraCollectibles/entity/Orden.java
@@ -1,31 +1,25 @@
 package Ishimura.uade.IshimuraCollectibles.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.Data;
-
-@Entity
 @Data
-@Table(name = "orden de compra")
+@Entity
+@Table(name = "ordenes")
 public class Orden {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
-    private Long count;
-
+    //un usuario puede generar muchas órdenes (1:N)
     @ManyToOne
-    @JoinColumn(name = "usuario_id") //Crea una columna usuario_id en la tabla orden en vez de una tabla separada para la relacion
-    private Usuario usuario;         //como en @ManyToMany
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    // una orden puede tener muchos Items
+    @OneToMany(mappedBy = "orden")
+    private List<OrdenItem> items = new ArrayList<>();
 }
